5dd9a3a02ef1b26078ea3f44cf36be2458dd9404
Jukka Miettinen <jukka.miettinen@nitor.fi>
2018-02-07 12:08:25 +0200

    Version number fix

--- Unscrabble/ViewController.swift
+++ Unscrabble/ViewController.swift
@@ -87,6 +87,10 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDataSour
         let matchingWord: String
         matchingWord = matchingWords[indexPath.row]
         cell.textLabel!.text = matchingWord
+
+        cell.textLabel!.attributedText = NSAttributedString(string: "cell.textLabel!.text", attributes:
+            [.underlineStyle: NSUnderlineStyle.styleSingle.rawValue])
+
         return cell
     }
 
